bab49a05cf968ae559a6ef5923a593b8e2bc109b
Alice Aquino <53798258+lic3as@users.noreply.github.com>
2025-01-26 10:21:09 -0300

    component not found

--- cafe/src/components/NotFound.jsx
+++ cafe/src/components/NotFound.jsx
@@ -1,1 +1,15 @@
 //página pra quando errarem o caminho (404 - página não encontrada)
+import React from "react";
+import './NotFound.css';
+
+const NotFound = () => {
+    return(
+        <>
+        <h2>404 - NOT FOUND</h2>
+        <h4>Ops, a página que você está procurando não existe!</h4>
+        </>  
+    )
+    
+}
+
+export default NotFound;
