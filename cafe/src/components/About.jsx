c0c6d281b0d4e02e7cc5b42e3a181101f5e723e0
Alice Aquino <53798258+lic3as@users.noreply.github.com>
2025-01-27 09:12:14 -0300

    menu e ajustes

--- cafe/src/components/About.jsx
+++ cafe/src/components/About.jsx
@@ -4,7 +4,7 @@ import './About.css';
 
 const About = () => {
     return(
-        <div className="main">
+        <div className="main-about">
             <h2>Sobre Nós</h2>
             <p>O Café do Amanhã foi fundado por Nanda e Junior, dois apaixonados por café e pela cidade de Fortaleza. Localizado na Avenida Beira Mar, nosso café oferece um refúgio acolhedor onde você pode desfrutar de sabores únicos e de uma vista deslumbrante.</p>
             <div className="carrossel">
--- cafe/src/components/NotFound.jsx
+++ cafe/src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import './NotFound.css';
 
 const NotFound = () => {
     return(
-        <div className="main">
+        <div className="main-not-found">
             <h2 className="notFound">404 - NOT FOUND</h2>
             <h4>Ops, a página que você está procurando não existe!</h4>
         </div>  
