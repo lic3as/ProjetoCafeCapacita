995d0b7d553194255739253a5f25c7848108fce8
Natanael Melo <natanael.melo417@gmail.com>
2025-01-28 16:10:29 -0300

    feat: implementando rotas

--- cafe/src/App.jsx
+++ cafe/src/App.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import NotFound from './components/NotFound';
-import About from './components/About';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import NotFound from './components/NotFound';
+import About from './components/About';
 import Menu from './components/Menu';
 import Hero from './components/Hero';
+import OrderPage from './components/OrderPage';
 import Contact from './components/Contact';
 
 function App() {
-  return(
+  return (
     <>
-    <Router>
-      <Header></Header>
-      <Routes>
-        <Route path='/sobre-nos' element={<About></About>}></Route>
-        <Route path='/menu' element={<Menu></Menu>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-        <Route path='/contact' element={<Contact></Contact>}></Route>
-      </Routes>
-      <Footer></Footer>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" index element={<Hero />} />
+          <Route path="/sobre-nos" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/pedidos" element={<OrderPage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/contato" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
