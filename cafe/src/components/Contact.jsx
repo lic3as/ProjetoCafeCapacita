8ecab9b931769bd4ad74d54221106bbb6b552a83
Natanael Melo <natanael.melo417@gmail.com>
2025-01-28 16:10:17 -0300

    fix: corrigindo bugs e estilização

--- cafe/src/components/Contact.jsx
+++ cafe/src/components/Contact.jsx
@@ -1,32 +1,42 @@
-//página de contato
+// página de contato
 import React from 'react';
-import "./Contact.css";
+import './Contact.css';
 
 const Contact = () => {
-    return (
-        <div class="container" >
-            <h2>Contato</h2>
-            <p>
-                Estamos ansiosos para ouvir você! Entre em contato consosco através dos detalhes abaixo ou envie-nos uma mensagem diretamente pelo formulário
-            </p>
-            <p><strong>Endereço:</strong>738 Av. Beira Mar, Fortaleza, CE</p>
-            <p><strong>Telefone:</strong> (85) 1234-5678</p>
-            <p><strong>Email:</strong>contato@cafedoamanha.com</p>
-            <form action="https://formsubmit.co/contatocafecapacita@gmail.com" method="POST" class="form-contato">
+  return (
+    <div className="container-contato">
+      <h2>Contato</h2>
+      <p>
+        Estamos ansiosos para ouvir você! Entre em contato conosco através dos
+        detalhes abaixo ou envie-nos uma mensagem diretamente pelo formulário
+      </p>
+      <p>
+        <strong>Endereço:</strong> 738 Av. Beira Mar, Fortaleza, CE
+      </p>
+      <p>
+        <strong>Telefone:</strong> (85) 1234-5678
+      </p>
+      <p>
+        <strong>Email:</strong> contato@cafedoamanha.com
+      </p>
+      <form
+        action="https://formsubmit.co/contatocafecapacita@gmail.com"
+        method="POST"
+        className="form-contato"
+      >
+        <label htmlFor="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required />
 
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required />
-            
-            <label for="email">Email:</label>
-            <input type="text" id="email " name="email" required />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
 
-            <label for="mensagem">Mensagem: </label>
-            <textarea name="mensagem" rows="4" id="mensagem" required ></textarea>
+        <label htmlFor="mensagem">Mensagem: </label>
+        <textarea name="mensagem" rows="4" id="mensagem" required></textarea>
 
-            <button type="submit">Enviar</button>
-            </form>
-        </div>
-    );
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
--- cafe/src/components/Hero.jsx
+++ cafe/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css';
 function Hero() {
   return (
     <section className="hero d-flex align-items-center justify-content-center">
-      <div className="container text-center">
+      <div className="container text-center container-hero">
         <h2>Cada xícara conta uma história</h2>
         <p>
           Bem-vindo ao Café do Amanhã, onde o aroma do café se mistura com a
